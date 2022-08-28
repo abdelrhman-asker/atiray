9e96e1d7a63366e68441d3b309fc840d89a43057
unknown <abdosker11@yahoo.com>
2022-08-28 19:54:26 +0200

    LastChanges

--- main.js
+++ main.js
@@ -14,7 +14,34 @@ function randombg(){
   function RemoveNavS() {
     document.querySelector(".Rightnav").style.display = "none"
     document.querySelector(".transparentDiv").style.display = "none"
-
     document.querySelector(".Based").style.marginRight = "0%"
 
   }
+
+
+var search_terms = ['apple', 'apple watch', 'apple macbook', 'apple macbook pro', 'iphone', 'iphone 12', 'google', 'query', 'save', 'door', 'playstation', 'laptop', 'zoomin', 'zoomout', 'inside', 'outSide', 'bandicam', 'noor', 'wrap', 'nowrap', 'repeat', 'cars', 'tree'];
+
+function autocompleteMatch(input) {
+  if (input == '') {
+    return [];
+  }
+  var reg = new RegExp(input)
+  return search_terms.filter(function(term) {
+	  if (term.match(reg)) {
+  	  return term;
+	  }
+  });
+}
+
+function showResults(val) {
+  res = document.getElementById("result");
+  res.innerHTML = '';
+  let list = '';
+  let terms = autocompleteMatch(val);
+  for (i=0; i<terms.length; i++) {
+    list += '<li>' + terms[i] + '</li>';
+    document.querySelector("#result").style.height= "210px"
+    document.querySelector("#result").style.overflowY= "hidden"
+  }
+  res.innerHTML = '<ul>' + list + '</ul>';
+}
