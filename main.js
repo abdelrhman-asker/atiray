93ffb15183cc57132d1a97276352379bc572f609
unknown <abdosker11@yahoo.com>
2022-08-28 14:20:51 +0200

    atiray

---
+++ main.js
@@ -0,0 +1,17 @@
+function randombg(){
+    var random= Math.floor(Math.random() * 3) + 0;
+    var bigSize = ["url('https://atiray.com/site/backgrounds/yosemite-1590012_1920.jpg')",
+                   "url('https://atiray.com/site/backgrounds/dunes-4033821_1920.jpg')",
+                   "url('https://atiray.com/site/backgrounds/hummingbird-2139278_1920.jpg')",];
+    document.querySelector(".MainBg").style.backgroundImage=bigSize[random];
+  }
+
+  function openSidebar() {
+    document.querySelector(".Rightnav").style.display = "flex"
+    document.querySelector(".Based").style.marginRight = "20%"
+  }
+  function RemoveNavS() {
+    document.querySelector(".Rightnav").style.display = "none"
+    document.querySelector(".Based").style.marginRight = "0%"
+
+  }
